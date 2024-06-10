1e610d06e9850340eaebfc0e10457ba93127281e
dhruv patel <dhruv160916@gmail.com>
2024-06-10 22:31:40 +0530

    Admin Register , Login and Protected Route Api Created

---
+++ middleware/admin.js
@@ -0,0 +1,27 @@
+// middlewares/isAuthenticated.js
+const jwt = require('jsonwebtoken');
+const Admin= require("../model/Admin");
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+
+exports.isAdmin = catchAsyncErrors(async(req,res,next) => {
+    // Get token from headers or query params or cookies
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(new ErrorHandler("Please login to continue", 401));
+    }
+
+        const decoded = jwt.verify(token, process.env.ADMIN_JWT_SECRET_KEY);
+        // Find admin by ID
+        const admin = await Admin.findById(decoded.id);
+
+    if (!admin) {
+      return next(new ErrorHandler("User not found", 404));
+    }
+  req.admin = admin;
+
+    next();
+});
+
+
